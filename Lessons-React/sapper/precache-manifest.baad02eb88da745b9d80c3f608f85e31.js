c06136e92238c5f9edf82ef814b98db08bc3b23c
Adam Suskhanov <maasovec@gmail.com>
2020-07-16 15:27:53 +0300

    add sapper

---
+++ Lessons-React/sapper/precache-manifest.baad02eb88da745b9d80c3f608f85e31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9863c7f2eb4361f02390173d1d07c274",
+    "url": "./index.html"
+  },
+  {
+    "revision": "48110df8a5f05d3fe24d",
+    "url": "./static/js/2.29115f8c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.29115f8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98a6437835feadd4c417",
+    "url": "./static/js/main.8516f17b.chunk.js"
+  },
+  {
+    "revision": "ea9c94dd73f26b8beb1a",
+    "url": "./static/js/runtime-main.33030309.js"
+  }
+]);
