a9ada1189f818b45453a344bd1685076e7416a53
Adam Suskhanov <maasovec@gmail.com>
2020-07-16 14:49:40 +0300

    add massive...

---
+++ Lessons-React/massive-filter-map/precache-manifest.d1536d011b9406e9d3e820e9369525bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c778aaaa3d1c027c31e0b2135dca694d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "33bdefca2c0ea00a7f93",
+    "url": "./static/js/2.5553d9fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.5553d9fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6877af652f937eadbcb",
+    "url": "./static/js/main.d1fe0c65.chunk.js"
+  },
+  {
+    "revision": "ea9c94dd73f26b8beb1a",
+    "url": "./static/js/runtime-main.33030309.js"
+  }
+]);
