0b68425f56da58728ff2aee84600543b27bab3a2
Adam Suskhanov <maasovec@gmail.com>
2020-07-16 14:43:13 +0300

    add toggle

---
+++ Lessons-React/toggle/precache-manifest.67ef8a2468766c08b6124b61590d07cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fe34bb477734065ed8fc23aa431903e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2b9231b2d60f410334d1",
+    "url": "./static/css/main.e53014fe.chunk.css"
+  },
+  {
+    "revision": "ab3ce883e5a684356fad",
+    "url": "./static/js/2.34d2b4f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.34d2b4f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b9231b2d60f410334d1",
+    "url": "./static/js/main.cc6bb1f6.chunk.js"
+  },
+  {
+    "revision": "ea9c94dd73f26b8beb1a",
+    "url": "./static/js/runtime-main.33030309.js"
+  }
+]);
