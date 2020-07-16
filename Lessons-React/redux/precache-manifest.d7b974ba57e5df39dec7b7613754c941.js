13a1e8de7582a45c08e930e2b21ff9b1d2723752
Adam Suskhanov <maasovec@gmail.com>
2020-07-16 15:14:49 +0300

    add redux

---
+++ Lessons-React/redux/precache-manifest.d7b974ba57e5df39dec7b7613754c941.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60ba01e913214f0f7e56d855c07c9a8a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2d71b1a4f057700b87fe",
+    "url": "./static/js/2.b9672912.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.b9672912.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87790de0f36615883353",
+    "url": "./static/js/main.9e6976af.chunk.js"
+  },
+  {
+    "revision": "ea9c94dd73f26b8beb1a",
+    "url": "./static/js/runtime-main.33030309.js"
+  }
+]);
