d6b06aecbc3c8eb9c1b0d0c1edc14f1fa7a57635
Adam Suskhanov <maasovec@gmail.com>
2020-07-16 15:23:47 +0300

    add dropList

---
+++ Lessons-React/dropList/precache-manifest.7570a4307ae7aacfdf9af9efaf72de4d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97c9540f224d6d3242df41694480711a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "48110df8a5f05d3fe24d",
+    "url": "./static/js/2.29115f8c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.29115f8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81476a1193c3ddf0bed7",
+    "url": "./static/js/main.09a30bd9.chunk.js"
+  },
+  {
+    "revision": "ea9c94dd73f26b8beb1a",
+    "url": "./static/js/runtime-main.33030309.js"
+  }
+]);
