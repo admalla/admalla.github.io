cbbe0bbbe183506d68e4e4213985f9fb43777c3c
Adam Suskhanov <maasovec@gmail.com>
2020-07-16 14:38:07 +0300

    add folder

---
+++ Lessons-React/counter/precache-manifest.41aa13e39142d063e4a227d67681f65b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d949219c373bb4ab4c4521789cdc0589",
+    "url": "./index.html"
+  },
+  {
+    "revision": "78745f31a9e454f03745",
+    "url": "./static/css/main.4ade386a.chunk.css"
+  },
+  {
+    "revision": "3ee7b5767e53be175fc7",
+    "url": "./static/js/2.bb685aab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.bb685aab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78745f31a9e454f03745",
+    "url": "./static/js/main.c61866b0.chunk.js"
+  },
+  {
+    "revision": "ea9c94dd73f26b8beb1a",
+    "url": "./static/js/runtime-main.33030309.js"
+  }
+]);
---
+++ Lessons-React/kurs/precache-manifest.b1299c8411e9259056930ac2b3b2c439.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21669517970e5db1a0a24f5ea277f339",
+    "url": "./index.html"
+  },
+  {
+    "revision": "18d8fd49c493d322e89c",
+    "url": "./static/js/2.0cc4ac14.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.0cc4ac14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d811419bf3bead638766",
+    "url": "./static/js/main.f302a07a.chunk.js"
+  },
+  {
+    "revision": "ea9c94dd73f26b8beb1a",
+    "url": "./static/js/runtime-main.33030309.js"
+  }
+]);
